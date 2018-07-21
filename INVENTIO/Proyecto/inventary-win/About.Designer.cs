b7430e32bbed748f5d19f750a18f7fa5017adb3d
danielorozco14 <00200617@uca.edu.sv>
2018-07-20 19:13:47 -0600

    Wey mira como va :v lee los comments del Login.cs

---
+++ INVENTIO/Proyecto/inventary-win/About.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace inventio_win
+{
+    partial class About
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Inventory System";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Version Alpha";
+            // 
+            // label3
+            //
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(243, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Creditos: Daniel Orozco - Desarrollador";
+            
+            // 
+            // label4
+            
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(160, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Creditos: Roberto Rubio - Desarrollador";
+            
+       
+            // 
+            // About
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 208);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "About";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Acerca de Inventory System";
+            this.Load += new System.EventHandler(this.About_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
---
+++ INVENTIO/Proyecto/inventary-win/Connection.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace inventio_win
+{
+    class Connection
+    {
+        public MySqlConnection con;
+        public long insert_id;
+        public Connection() {
+            try
+            {
+                MySqlConnectionStringBuilder b = new MySqlConnectionStringBuilder();
+                b.Server = "localhost";
+                b.UserID = "root";//usuario que tenes registrado en tu base de datos mysql, no vayas a ocupar xampp que no sirve
+                b.Password = "140799Daoo";//contrasenia que le pusiste a la base de datos de mysql
+                b.Database = "inventario";
+                con = new MySqlConnection(b.ToString());
+            }
+            catch (MySqlException me) {
+                MessageBox.Show(me.Message);
+            }
+        }
+
+        public void execute(String sql) {
+            MySqlCommand cmd = this.con.CreateCommand();
+            cmd.CommandText = sql;
+            this.con.Open();
+            cmd.ExecuteNonQuery();
+            this.con.Close();
+            this.insert_id = cmd.LastInsertedId;
+        }
+
+
+    }
+}
