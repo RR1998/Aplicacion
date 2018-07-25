bbe240e02bed3f09ae7b3b3296fbf689ba799afd
Roberto Rubio <00259517@uca.edu.sv>
2018-07-25 12:20:08 -0600

    Cambios en coneccion para que se haga con un switch case y tambien tome en cuenta los usuario, etc

--- INVENTIO/Proyecto/inventary-win/Connection.cs
+++ INVENTIO/Proyecto/inventary-win/Connection.cs
@@ -5,28 +5,26 @@ using System.Text;
 using MySql.Data.MySqlClient;
 using System.Windows.Forms;
 
-namespace inventio_win
-{
-    class Connection
-    {
+namespace inventio_win{
+    class Connection{
         public MySqlConnection con;
         public long insert_id;
-        public Connection() {
-            try
-            {
-                MySqlConnectionStringBuilder b = new MySqlConnectionStringBuilder();
-                b.Server = "localhost";
-                b.UserID = "root";//usuario que tenes registrado en tu base de datos mysql, no vayas a ocupar xampp que no sirve
-                b.Password = "140799Daoo";//contrasenia que le pusiste a la base de datos de mysql
-                b.Database = "inventario";
-                con = new MySqlConnection(b.ToString());
-            }
-            catch (MySqlException me) {
-                MessageBox.Show(me.Message);
-            }
+        public Connection(String User, String Password) {
+            switch (User) {
+                //Cambiar para que el case lo haga por el usuario buscando en la base no por el dato quemado
+                case ("Admin"):
+                    Coneccion(User, Password);
+                    break;
+                case ("Usuario"):
+                    Coneccion(User, Password);
+                    break;
+                default:
+                    MessageBox.Show("El usuario" + User + "No esta registrado");
+                    break;
+            } 
         }
 
-        public void execute(String sql) {
+        public void execute(String sql){
             MySqlCommand cmd = this.con.CreateCommand();
             cmd.CommandText = sql;
             this.con.Open();
@@ -34,7 +32,20 @@ namespace inventio_win
             this.con.Close();
             this.insert_id = cmd.LastInsertedId;
         }
-
-
+        public void Coneccion(String User, String Password){
+            try
+            {
+                MySqlConnectionStringBuilder b = new MySqlConnectionStringBuilder();
+                b.Server = "localhost";
+                b.UserID = User;//usuario que tenes registrado en tu base de datos mysql, no vayas a ocupar xampp que no sirve
+                b.Password = Password;//contrasenia que le pusiste a la base de datos de mysql
+                b.Database = "Proyecto";
+                con = new MySqlConnection(b.ToString());
+            }
+            catch (MySqlException me)
+            {
+                MessageBox.Show(me.Message);
+            }
+        }
     }
 }
